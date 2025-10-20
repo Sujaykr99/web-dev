5fe7a739c9b042ce968dc471bc8a10d1f32de5a8
sujay99 <sujaykrsingh999@gmail.com>
2025-10-20 06:52:02 +0530

    Initial commit

---
+++ project 1/main.js
@@ -0,0 +1,37 @@
+// countdown timer
+const endDate = new Date(" 29 sep , 2025 1:10:00").getTime() ;
+const startDate = new Date().getTime();
+
+ let x = setInterval( function updateTimer(){
+
+const now = new Date().getTime();
+
+const distancecovered = now - startDate;
+const distancepending = endDate - now ;
+//calculate days , min , hrs , secs 
+
+const  days =  Math.floor(distancepending/(24*60*60*1000));
+const hrs = Math.floor((distancepending%(24*60*60*1000)/(60*60*1000))) ;
+const mins = Math.floor((distancepending%(60*60*1000)/(60*1000)));
+const secs = Math.floor((distancepending%(60*1000)/(1000))) ;
+
+//populate in ui
+document.getElementById("days").innerHTML = days ;
+document.getElementById("hours").innerHTML = hrs ;
+document.getElementById("minutes").innerHTML = mins ;
+document.getElementById("seconds").innerHTML = secs ;
+
+const totalDistance = endDate - startDate ;
+const percentageDistance = (distancecovered/totalDistance)*100 ;
+
+// set weith by progess bar 
+document.querySelector(".progress-bar").style.width = percentageDistance + "%" ;
+
+ if (distancepending <= 0) {
+    clearInterval(x);
+    document.querySelector(".container").innerHTML = "EXPIRED";
+    document.querySelector(".progress-bar").style.width = "100%";
+    return;
+  }
+
+}, 1000) ;
